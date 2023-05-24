ac2eda59b1d98bff3319b112fb71114e8fb4aeb1
Lucas756 <salleslucas00@gmail.com>
2023-05-23 21:33:38 -0300

    alteração

--- src/app.js
+++ src/app.js
@@ -36,8 +36,7 @@ class App {
 
         return res.status(500).json(errors);
       }
-
-      return res.status(500).json({ error: 'Erro de teste' });
+      return res.status(500).json({ error: 'Erro de testeeeeeeeeee' });
     });
   }
 }
